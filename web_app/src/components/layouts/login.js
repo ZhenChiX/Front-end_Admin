c987d2d9087c393cbecad5a4385f6c3aa67de128
StarGale <zhenchix@hotmail.com>
2019-03-21 12:22:26 -0700

    landing page layout almost done

--- web_app/src/components/layouts/login.js
+++ web_app/src/components/layouts/login.js
@@ -5,20 +5,28 @@ import Card from "@material-ui/core/Card";
 import { TextField, CardHeader, CardMedia } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-
+import ReactDOM from "react-dom";
+//login input
 import InputAdornment from "@material-ui/core/InputAdornment";
 import MenuItem from "@material-ui/core/MenuItem";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
-
+// icon
 import IconButton from "@material-ui/core/IconButton";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import TrackChange from "@material-ui/icons/TrackChanges";
+// import Settings from "@material-ui/icons/Settings";
 
-const styles = theme => ({
+// tab view
+// import SwipeableViews from "react-swipeable-views";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
   container: {
     display: "grid",
-    // flexWrap: "wrap"
     gridTemplateColumns: "1fr"
   },
   textField: {
@@ -33,113 +41,277 @@ const styles = theme => ({
   },
   menu: {
     width: 300,
-    position: "relative",
+    position: "absolute",
     margin: "auto",
-    overflow: "visible"
+    overflow: "visible",
+    top: "20vh"
   },
+  tabs: {
+    width: 280,
+    margin: "auto",
+    position: "relative"
+  },
+
   grid: {
     position: "relative"
   },
   media: {
-    backgroundImage:
-      "url(https://img.talkandroid.com/uploads/2014/03/sprint-logo-630x330.jpg)",
-    // ⚠️ object-fit is not supported by IE 11.
-
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    height: "120px"
+    height: "120px",
+    width: "280px",
+    margin: "auto",
+    top: "-0.3em",
+    position: "relative",
+    zIndex: "1",
+    boxShadow: "3px 3px 10px 2px rgba(128, 128, 128, 0.7)"
   },
   spin: {
     left: "-0.3em",
     top: "-0.3em",
-    color: "rgba(245,205,5,0.9)",
+    color: "rgba(245,205,5,0.8)",
     position: "absolute",
-    fontSize: "80px"
+    fontSize: "80px",
+    animation: "App-logo-spin infinite 10s linear",
+    zIndex: "99"
   }
-});
+};
+
+//tab container
+function TabContainer({ children, dir }) {
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       multiline: "Controlled",
       password: "",
-      showPassword: false
+      confirmPassword: "",
+      showPassword: false,
+
+      value: 0
     };
   }
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
+
+  componentDidMount() {}
+  componentWillUpdate() {}
+  handleChange = name => e => {
+    this.setState({ [name]: e.target.value });
   };
 
-  handleMouseOver = e => {};
+  handleTabChange = (event, value) => {
+    this.setState({ value });
+  };
 
+  handleTabChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+  //   handleMouseOver = e => {
+  //     //   logo spin animation on hover
+  //     const node = ReactDOM.findDOMNode(this);
+  //     if (node instanceof HTMLElement) {
+  //       const target = node.querySelector("#App-logo");
+  //       target.style.animation = "App-logo-spin-hover infinite 2s linear";
+  //     }
+  //   };
+  //   handleMouseLeave = e => {
+  //     //   logo spin animation on leave
+  //     const node = ReactDOM.findDOMNode(this);
+  //     if (node instanceof HTMLElement) {
+  //       const target = node.querySelector("#App-logo");
+  //       target.style.animation = "App-logo-spin infinite 10s linear";
+  //     }
+  //   };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   handleClickShowPassword = () => {
     this.setState(state => ({ showPassword: !state.showPassword }));
   };
+
+  handleRotate = e => {};
   render() {
     const { classes } = this.props;
     return (
       <Fragment>
-        {/* <Card className={classes.outerCard}> */}
-        <Card className={classes.menu}>
+        <Card id="App-card" className={classes.menu}>
           <Grid item className={classes.grid}>
             <TrackChange
               id="App-logo"
               className={classes.spin}
               color="primary"
+              spin={this.state.spin}
             />
-
-            <CardMedia className={classes.media} alt="Sprint Logo" />
-          </Grid>
-          <form className={classes.container} noValidate autoComplete="off">
-            <TextField
-              required
-              id="login_username"
-              label="UserName/Email"
-              className={classes.textField}
-              onChange={this.handleChange("name")}
-              margin="normal"
-              // variant="filled"
-              InputProps={{
-                endAdornment: <InputAdornment position="end" />
-              }}
-            />
-
-            <TextField
-              required
-              id="login_password"
-              className={classes.textField}
-              type={this.state.showPassword ? "text" : "password"}
-              label="Password"
-              value={this.state.password}
-              onChange={this.handleChange("password")}
-              // variant="filled"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="Toggle password visibility"
-                      onClick={this.handleClickShowPassword}
-                    >
-                      {this.state.showPassword ? (
-                        <VisibilityOff />
-                      ) : (
-                        <Visibility />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <Button
+            {/* <Settings
+              id="App-logo"
+              className={classes.spin}
               color="primary"
-              id="App-login"
-              onMouseOver={this.handleMouseOver}
+              spin={this.state.spin}
+            /> */}
+
+            <CardMedia
+              id="App-banner"
+              className={classes.media}
+              image="https://img.talkandroid.com/uploads/2014/03/sprint-logo-630x330.jpg"
+              //   image="https://preview.redd.it/izy5wrj4st221.png?width=960&crop=smart&auto=webp&s=2da2da0e512c6dfd15ade892440dc6b12c54afa3"
+              // image="https://via.placeholder.com/630x330?text=SAuto+Placeholder+Banner"
+              alt="Sprint Logo"
+            />
+          </Grid>
+
+          <AppBar className={classes.tabs} position="static" color="default">
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleTabChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant="fullWidth"
             >
-              Login
-            </Button>
-          </form>
+              <Tab label="Login" />
+              <Tab label="SignUp" />
+            </Tabs>
+          </AppBar>
+          {this.state.value === 0 && (
+            <TabContainer>
+              <form
+                className={classes.container}
+                onSubmit={this.handleSubmit}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  required
+                  id="login_username"
+                  label="UserName/Email"
+                  className={classes.textField}
+                  onChange={this.handleChange("name")}
+                  margin="normal"
+                />
+
+                <TextField
+                  required
+                  id="login_password"
+                  className={classes.textField}
+                  type={this.state.showPassword ? "text" : "password"}
+                  label="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange("password")}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  //   color="primary"
+                  id="App-login"
+                  onMouseOver={this.handleMouseOver}
+                  onMouseLeave={this.handleMouseLeave}
+                >
+                  Login
+                </Button>
+              </form>
+            </TabContainer>
+          )}
+
+          {this.state.value === 1 && (
+            <TabContainer>
+              <form
+                className={classes.container}
+                onSubmit={this.handleSubmit}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  required
+                  id="signup_username"
+                  label="UserName/Email"
+                  className={classes.textField}
+                  onChange={this.handleChange("name")}
+                  margin="normal"
+                />
+
+                <TextField
+                  required
+                  id="signup_password"
+                  className={classes.textField}
+                  type={this.state.showPassword ? "text" : "password"}
+                  label="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange("password")}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <TextField
+                  required
+                  id="confirm_password"
+                  className={classes.textField}
+                  type={this.state.showPassword ? "text" : "password"}
+                  label="Confirm Password"
+                  value={this.state.confirmPassword}
+                  onChange={this.handleChange("confirmPassword")}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  id="App-register"
+                  onMouseOver={this.handleMouseOver}
+                  onMouseLeave={this.handleMouseLeave}
+                >
+                  Register
+                </Button>
+              </form>
+            </TabContainer>
+          )}
         </Card>
-        {/* </Card> */}
       </Fragment>
     );
   }
